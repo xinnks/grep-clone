2df6240c3ad5f3eeb3cc06f2ad7b548f26de1d73
James Sinkala <xinnks@gmail.com>
2020-11-10 01:17:42 +0300

    process arguments and display lines matching provided pattern

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,13 @@ struct Cli {
 }
 
 fn main() {
+    let args = Cli::from_args();
+    // println!("search for {:#?} in {:?}", args.pattern, args.path)
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
